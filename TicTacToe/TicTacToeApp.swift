b75ff813bf9f3cfe1d68e567eea4e8fb84bb9905
M <malikgacc@gmail.com>
2024-01-12 10:35:00 -0600

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Malik Abdelrahim on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
